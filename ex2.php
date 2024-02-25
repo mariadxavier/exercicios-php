bcb45b0d6518c66ec45833eace44ec71eff913d0
Madu Xavier <mariadudaxavier7@gmail.com>
2024-02-25 19:13:27 -0300

    add resolução exercicio 2

--- ex2.php
+++ ex2.php
@@ -8,9 +8,30 @@
 <body>
     <h1>Exercício 2:</h1>
     <p>Na usina de Angra dos Reis, os técnicos analisam a perda de massa de umm material radioativo. Sabendo-se que este perde 25% de sua masa a cada 30 segundos, crie um algoritmo que imprima o tempo necesario para que a massa desse material seja menos que 0.10</p>
+    <form action="ex2.php" method="get">
+        <label for="massa">Insira a massa em gramas do material radioativo:</label>
+        <input type="number" name="massa">
+        <button type="submit">Verificar tempo</button>
+    </form>
 
     <?php 
-        
+        $massa = $_GET['massa'];
+        $tempoEmSegundos = 0;
+
+        function perderMassa($massa, $tempoEmSegundos) {
+            $massa -= ($massa * 0.25);
+            $tempoEmSegundos += 30;
+
+            if ($massa < 0.10)
+                return $tempoEmSegundos;
+            else 
+                return perderMassa($massa, $tempoEmSegundos);
+        }
+
+        $tempoFinal = perderMassa($massa, $tempoEmSegundos);
+
+
+        echo "<br>O tempo necessário para que a massa inserida seja menor que 0.10 é de $tempoFinal segundos, ou ". $tempoFinal/60 . " minutos";
     ?>
     
 </body>
