acd13fc511f8886cbbe9e7e91e6950079444dd42
Madu Xavier <mariadudaxavier7@gmail.com>
2024-02-25 21:43:18 -0300

    add form ex9

--- ex9.php
+++ ex9.php
@@ -10,6 +10,16 @@
     <p>Usando POST, crie um formulário que receba um login e senha de um usuário. Faça a validação dos campos de modo que o usuário tenha que preencher todos os campos. Se  o login for "etec" e a senha "informatica", exiba uma mensagem na tela de "logado com sucesso" e não exiba mais o formulário de login.</p>
     <hr>
 
+    <form action="ex9.php" method="post">
+        <label for="login">Login:</label>
+        <input type="text" name="login">
+        
+        <label for=""></label>
+        <input type="password" name="senha">
+
+        <button type="submit">Enviar</button>
+    </form>
+
     <?php 
         
     ?>
