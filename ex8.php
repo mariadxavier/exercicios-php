0839d04813aca7fb14fc730344571f5b617feb7b
Madu Xavier <mariadudaxavier7@gmail.com>
2024-02-25 21:41:36 -0300

    add form ex 8

--- ex8.php
+++ ex8.php
@@ -9,6 +9,22 @@
     <h1>Exercício 8:</h1>
     <p>Usando o método GET, crie um formulário que receba o nome do estudante, 3 notas de 0 a 10 e calcule a média quando um botão de submit for pressionado. Faça a validação dos campos de modo que usuário tenha que preencher todos eles e todos numéricos.</p>
     <hr>
+
+    <form action="ex8.php" method="get">
+        <label for="nome">Insira seu nome:</label>
+        <input type="text" name="nome">
+
+        <label for="nota1">Primeira Nota:</label>
+        <input type="number" name="nota1" id="">
+
+        <label for="nota2">Segunda Nota:</label>
+        <input type="number" name="nota2" id="">
+
+        <label for="nota3">Terceira Nota:</label>
+        <input type="number" name="nota3" id="">
+
+        <button type="submit"></button>
+    </form>
     
     <?php 
         
