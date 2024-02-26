b0ac86455b0220a2d74b6599bd6905fb664fbd28
Madu Xavier <mariadudaxavier7@gmail.com>
2024-02-25 21:36:38 -0300

    add resolução exercício 6

--- ex6.php
+++ ex6.php
@@ -7,10 +7,38 @@
 </head>
 <body>
     <h1>Exercício 6:</h1>
-    <p>Crie um algoritmo com uma matriz 5x5 e escreva os elementos da diagnal principal. Liste dos npumeros pares com a localização na matriz.</p>
+    <p>Crie um algoritmo com uma matriz 5x5 e escreva os elementos da diagnal principal. Liste dos números pares com a localização na matriz.</p>
     <hr>
     <?php 
-        
+
+    $matriz = [];
+    for ($i = 0; $i < 5; $i++) {
+        for ($j = 0; $j < 5; $j++) {
+            $matriz[$i][$j] = rand(0, 100); // Valores aleatórios de 1 a 100
+        };
+    };       
+
+    echo "Matriz: <br>";
+    foreach ($matriz as $linha) {
+        foreach ($linha as $elemento) {
+         echo $elemento . " ";
+        };
+        echo "<br>";
+    };
+    
+    echo "<br>Diagonal Principal: <br>";
+    for ($i = 0; $i < 5; $i++) {
+        echo $matriz[$i][$i] . "<br>";
+    };
+
+    echo "<br>Elementos pares e localização: <br>";
+    for ($i = 0; $i < 5; $i++) {
+        for ($j = 0; $j < 5; $j++) {
+            if ($matriz[$i][$j] % 2 == 0) {
+                echo "Elemento: " . $matriz[$i][$j] . " - Localização: ($i, $j)<br>";
+            };
+        };
+    };
     ?>
     
 </body>
